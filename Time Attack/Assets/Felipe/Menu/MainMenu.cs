51bcab5c53ddc6665229cd8359848feb54e78d23
covardecaocoragem <ukfelipelopes@gmail.com>
2025-12-16 12:29:32 +0000

    Most UI

---
+++ Time Attack/Assets/Felipe/Menu/MainMenu.cs
@@ -0,0 +1,44 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject settingsPanel;
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SettingButton()
+    {
+        if (settingsPanel.activeInHierarchy)
+        {
+            settingsPanel.SetActive(false);
+        }
+        else
+        {
+            settingsPanel.SetActive(true);
+        }
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void Awake()
+    {
+        settingsPanel.SetActive(false);
+    }
+    public void Update()
+    {
+        int Scene = SceneManager.GetActiveScene().buildIndex;
+        if (Scene == 0)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}

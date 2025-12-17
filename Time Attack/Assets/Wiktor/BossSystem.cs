0618770896f42fc7019f1fb56241dfa8544bb40e
victrix12345 <samborski09@gmail.com>
2025-12-17 15:07:19 +0000

    experimenting with IEnums to get couroutined attacks

--- Time Attack/Assets/Wiktor/BossSystem.cs
+++ Time Attack/Assets/Wiktor/BossSystem.cs
@@ -6,6 +6,8 @@ public class BossSystem : MonoBehaviour
 {
     GameObject player;
     bool canAttack = true;
+    int prevAtk = -1;
+    float atkCooldown;
     private void Awake()
     {
         player = GameObject.Find("Player");
@@ -13,6 +15,8 @@ public class BossSystem : MonoBehaviour
     void Update()
     {
         FacePlayer();
+        if (!canAttack) return;
+        StartCoroutine(AttackRoutine());
     }
     void FacePlayer()
     {
@@ -20,5 +24,26 @@ public class BossSystem : MonoBehaviour
         Vector3 lookPoint = (player.transform.position - transform.position) - new Vector3(0f,player.transform.position.y - transform.position.y,0f);
         rotation = Vector3.RotateTowards(transform.forward, lookPoint, 10f, 0f);
         transform.rotation = Quaternion.LookRotation(rotation);
+    }
+    IEnumerator AttackRoutine()
+    {
+        canAttack = false;
+        int selection = attackRoll();
+
+        switch (selection)
+        {
+            case 0: Debug.Log("Attack 0"); break;
+            case 1: Debug.Log("Attack 1"); break;
+        }
+        yield return new WaitForSeconds(atkCooldown);
+        canAttack = true;
+    }
+    int attackRoll()
+    {
+        int num;
+        do { num = Random.Range(0, 1); }
+        while (num == prevAtk);
+        prevAtk = num;
+        return num;
     }
 }

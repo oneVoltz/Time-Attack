54b46f9b7b2967f3c197dbac1e7ee7f67262623e
covardecaocoragem <ukfelipelopes@gmail.com>
2025-12-19 17:55:01 +0000

    a

--- Time Attack/Assets/Felipe/Menu/MainMenu.cs
+++ Time Attack/Assets/Felipe/Menu/MainMenu.cs
@@ -1,4 +1,4 @@
-using UnityEditor.SearchService;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
--- Time Attack/Assets/Yasmin/BossJumpAroundPlayer.cs
+++ Time Attack/Assets/Yasmin/BossJumpAroundPlayer.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using System.Collections;
-using UnityEditor.Experimental.GraphView;
+
 
 [RequireComponent(typeof(Rigidbody))]
 public class BossJumpAroundPlayer : MonoBehaviour
